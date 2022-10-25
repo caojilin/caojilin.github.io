a8d8152a4b3b763aa8e0bec5e53d74dc8dfcac15
tao <caojilin@berkeley.edu>
2022-10-24 22:42:02 -0700

    update

--- demo/maze_generator/sketch.js
+++ demo/maze_generator/sketch.js
@@ -1,138 +1,230 @@
 /*
 https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_depth-first_search
 */
-//width of a cell
-let w = 10
-let cols = 50
-let rows = 50
-let grid = []
-let stack = []
-let fps = 60
 
-function setup(){
-	createCanvas(w*cols, w*rows)
-	for (var i = 0; i < rows; i++) {
-		grid.push([])
-		for (var j = 0; j < cols; j++){
-			grid[i].push(new Cell(i, j))
-		}
+
+
+let width = 600;
+let cols, rows;
+let w = 30;
+let grid = [];
+let current;
+let stack;
+let line_width = 1
+let auto_run = false;
+
+function setup() {
+		createCanvas(width, width);
+		initilize()
+    button = createButton('instant result');
+		button.position(width+50, 100);
+		button.mousePressed(instant_finish);
+		button = createButton('auto');
+		button.position(width+50, 130);
+		button.mousePressed(auto);
+
+		inp = createInput('');
+		inp.position(width+50, 150);
+		inp.size(100);
+		inp.input(myInputEvent);
+}
+
+function myInputEvent(){
+	w = width/this.value()
+	if(w == Infinity || w == NaN){
+		return
 	}
-	frameRate(fps)
-	start_x = 0
-	start_y = 0
-	cur_cell = grid[start_x][start_y]
-    stack.push(cur_cell)
+	initilize()
+	loop()
 }
 
-function draw(){
-	background(220)
-	for (var i = 0; i < rows; i++){
-		for (var j = 0; j < cols; j++){
-			grid[i][j].show()
-		}
-	}
-	// dfs()
-    dfs_instant()
+function auto(){
+	auto_run = !auto_run;
 }
 
-function dfs() {
-	cur_cell.visited = true
-	cur_cell.highlight()
-	next_cell = cur_cell.checkNeighbors()
-	if(next_cell){
-		stack.push(next_cell)
-		removeWalls(cur_cell, next_cell)
-		next_cell.visited = true
-		cur_cell = next_cell
-	}else if (stack.length > 0) {
-		cur_cell = stack.pop()
-	}
-}
 
-function dfs_instant(){
-    while(stack.length > 0){
-      cur_cell.visited = true
-      cur_cell.highlight()
-      next_cell = cur_cell.checkNeighbors()
-      if(next_cell){
-          stack.push(next_cell)
-          removeWalls(cur_cell, next_cell)
-          next_cell.visited = true
-          cur_cell = next_cell
-      }else if (stack.length > 0) {
-          cur_cell = stack.pop()
-      }
-    }
-}
-
-function Cell(i, j) {
-	this.i = i
-	this.j = j
-	this.visited = false
-	this.walls = [true, true, true, true]
-	directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
-	this.checkNeighbors = function(){
-		neighbors = []
-		for (let dire of directions){
-			x = dire[0]
-			y =  dire[1]
-			n_x = this.i + x
-			n_y = this.j + y
-			can_visit = n_x >= 0 && n_x < rows && n_y >= 0 && n_y < cols
-			if (can_visit && !grid[n_x][n_y].visited){
-				neighbors.push(grid[n_x][n_y])
+function initilize(){
+	grid = []
+	cols = floor(width / w);
+	rows = floor(height / w);
+	for (let j = 0; j < rows; j++) {
+			for (let i = 0; i < cols; i++) {
+					grid.push(new Cell(i, j));
 			}
-		}
-		if (neighbors.length > 0) {
-			r = floor(random(0, neighbors.length))
-			return neighbors[r]
-		}else{
-			return undefined
-		}
 	}
-	this.show = function(){
-		x = this.j * w
-		y = this.i * w
-		stroke('black')
-		if (this.walls[0]) {
-			line(x, y, x+w, y)
-		}
-		if (this.walls[1]) {
-			line(x, y+w, x+w, y+w)
-		}
-		if (this.walls[2]) {
-			line(x, y, x, y+w)
-		}
-		if (this.walls[3]) {
-			line(x+w, y, x+w, y+w)
-		}
-	}
-	this.highlight = function (){
-		x = this.j * w
-		y = this.i * w
-		noStroke()
-		fill('green')
-		ellipseMode(CENTER)
-		ellipse(x + w/2, y + w/2, w/2, w/2)
+	current = grid[0];
+	stack = [current];
+}
+
+function instant_finish() {
+    //STEP 1.1
+    while (stack.length > 0) {
+        //STEP 2.2
+        current = stack.pop();
+        current.highlight();
+        let neighbors = current.getNeighbors();
+				let next = neighbors[floor(random(0, neighbors.length))];
+        if (next) {
+            //STEP 2.2.1
+            stack.push(current);
+            //STEP 2.2.3
+            removeWalls(current, next);
+            //STEP 2.2.4
+            next.visited = true;
+            stack.push(next);
+        }
+    }
+		line_width = 1
+}
+
+function run_once(){
+	//STEP 1.1
+	current.visited = true;
+	current.highlight();
+
+
+	//STEP 2.2
+	let neighbors = current.getNeighbors();
+	
+	let next = neighbors[floor(random(0, neighbors.length))];
+	if (next) {
+			//STEP 2.2.1
+			stack.push(next)
+			//STEP 2.2.3
+			removeWalls(current, next);
+			//STEP 2.2.4
+			next.visited = true;
+			current = next;
+	} else if (stack.length > 0) {
+			current = stack.pop();
 	}
 }
 
-function removeWalls(a, b){
-	x = a.j - b.j
-	if (x == 1) {
-		a.walls[2] = false
-		b.walls[3] = false
-	}else if (x == -1){
-		a.walls[3] = false
-		b.walls[2] = false
+function draw() {
+    background(220);
+		if(auto_run){
+			run_once()
+		}		
+    for (let cell of grid) {
+        cell.show();
+    }
+		draw_boundry()
+		current.visited = true;
+		current.highlight();
+		if(stack.length == 0){
+			noLoop()
+		}
+}
+
+function draw_boundry(){
+	line(0, 0, width, 0)
+	line(0, 0, 0, width)
+	line(width,0, width, width)
+	line(0, width, width, width)
+}
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+		run_once()
 	}
-	y = a.i - b.i
-	if (y == 1) {
-		a.walls[0] = false
-		b.walls[1] = false
-	}else if (y == -1){
-		a.walls[1] = false
-		b.walls[0] = false
 	}
+
+	
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+        return -1;
+    }
+    return i + j * cols;
+}
+
+class Cell {
+	constructor(i, j) {
+		this.i = i;
+		this.j = j;
+		this.visited = false;
+		//top, right, bottom, left
+		this.walls = [true, true, true, true];
+		this.neighbors;
+
+		this.getNeighbors = function () {
+			let neighbors = [];
+			let top = grid[index(i, j - 1)];
+			let right = grid[index(i + 1, j)];
+			let bottom = grid[index(i, j + 1)];
+			let left = grid[index(i - 1, j)];
+
+			if (top && !top.visited) {
+				neighbors.push(top);
+			}
+			if (right && !right.visited) {
+				neighbors.push(right);
+			}
+			if (bottom && !bottom.visited) {
+				neighbors.push(bottom);
+			}
+			if (left && !left.visited) {
+				neighbors.push(left);
+			}
+			return(neighbors)
+		};
+		this.highlight = function () {
+			let x = this.i * w;
+			let y = this.j * w;
+			push();
+			noStroke();
+			fill('green');
+			ellipseMode(CENTER);
+			ellipse(x + w / 2, y + w / 2, w / 2, w / 2);
+			pop();
+		};
+
+		this.show = function () {
+			let x = this.i * w;
+			let y = this.j * w;
+			stroke('black');
+
+			if (this.walls[0]) {
+				strokeWeight(line_width)
+				line(x, y, x + w, y);
+			}
+			if (this.walls[1]) {
+				strokeWeight(line_width)
+				line(x + w, y, x + w, y + w);
+			}
+			if (this.walls[2]) {
+				strokeWeight(line_width)
+				line(x + w, y + w, x, y + w);
+			}
+			if (this.walls[3]) {
+				strokeWeight(line_width)
+				line(x, y + w, x, y);
+			}
+			if (this.visited) {
+				push();
+				noStroke();
+				fill(220);
+				rect(x, y, w, w);
+				pop();
+			}
+		};
+	}
+}
+
+function removeWalls(a, b) {
+    let x = a.i - b.i;
+    if (x === 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x === -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+    let y = a.j - b.j;
+    if (y === 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y === -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
+    }
 }
